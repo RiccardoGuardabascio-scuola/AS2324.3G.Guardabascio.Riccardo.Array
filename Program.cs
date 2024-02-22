688bbedec2ab2cbb81679e8ef3a4728d5c1b93f2
PC <PC@INFO5-2561>
2024-02-22 09:02:27 +0100

    feat: Aggiunta funzione CaricaVettori

--- Program.cs
+++ Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            Console.WriteLine("Inserire il numero di persone");
+            int persone=Convert.ToInt32(Console.ReadLine());
+            int[] eta=new int[persone];
+            double[] pesi=new double[persone];
+            CaricaVettori(ref pesi, ref eta);
+           
+        }
+
+        static void CaricaVettori(ref double[] pesi, ref int[] eta)
+        {
+            Random random = new Random();
+            for(int i=0; i<pesi.Length; i++)
+            {
+                pesi[i] = random.Next(50, 101);
+                eta[i] = random.Next(18, 100);
+            }
         }
     }
 }
