5a67066c7cfde7b86c68cfcb6825229946964a39
PC <PC@INFO5-2561>
2024-02-22 08:50:00 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AS2324._3G.Guardabascio.Riccardo.Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+        }
+    }
+}
